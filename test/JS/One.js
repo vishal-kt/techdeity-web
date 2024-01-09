9f57f6d42f6dca80f043308d5e8f9f4ab6946f93
VISHAL <1316510155@kit.ac.in>
2024-01-09 12:42:28 +0530

    update

--- test/JS/One.js
+++ test/JS/One.js
@@ -7,4 +7,13 @@
 marvel_heros.pop(dc_heros)
 // console.log(marvel_heros);
 const heros= marvel_heros.concat(dc_heros)
-console.log(heros);
+// console.log(heros);
+
+
+const  all_heros = [...marvel_heros,...dc_heros]
+// console.table(all_heros);
+
+const another_array = [1,2,3,4,[7,6,8],[4,3,2,1,[4,5,6,7]]]
+
+const single_array =another_array.flat(Infinity)
+ console.log(single_array);
