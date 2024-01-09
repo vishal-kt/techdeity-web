c515662e00a9c86eddbb97a5f69b6a5cbc19a2c8
VISHAL <1316510155@kit.ac.in>
2024-01-09 11:54:30 +0530

    javascript update

---
+++ test/JS/One.js
@@ -0,0 +1,10 @@
+
+ const marvel_heros= ["iron man","thor","captain america","black panther","spider man"]
+ const dc_heros =["superman","flash","batman"]
+
+ marvel_heros.push(dc_heros)
+//  console.log(marvel_heros); 
+marvel_heros.pop(dc_heros)
+// console.log(marvel_heros);
+const heros= marvel_heros.concat(dc_heros)
+console.log(heros);
