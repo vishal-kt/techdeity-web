7791c611d40340bab4965c804516a6233327792e
VISHAL <1316510155@kit.ac.in>
2024-01-10 21:38:23 +0530

    update js

---
+++ test/JS/function/Three.js
@@ -0,0 +1,34 @@
+ function One() {
+    const Username = "vishal kumar";
+     
+    function Two() {
+            website = "youtube";
+        console.log(Username);
+ }
+    console.log(website);
+    Two();
+}
+// One();
+
+if(true){
+    const username = "vishal kumar";
+    if(username === "vishal kumar"){
+        const website = "   youtube";
+        // console.log(username + website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+
+function addOne(num) {
+    return num + 1;
+}
+
+console.log(addOne(5));
+const addTwo =function (num) {
+    return num +2 
+}
+
+console.log(addTwo(5)
+);
