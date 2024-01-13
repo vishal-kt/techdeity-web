bc09eb793b036c9cd881a839de484f804831a1da
VISHAL <1316510155@kit.ac.in>
2024-01-13 21:57:19 +0530

    update

--- test/JS/AdvanceJS/ProjectThree/script.js
+++ test/JS/AdvanceJS/ProjectThree/script.js
@@ -8,6 +8,7 @@ form.addEventListener('submit',function (e) {
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
+    const rr = document.querySelector('#rr')
 
     if ((height === '') || (height < 0) || (isNaN(height))) {
         document.querySelector('#height-error').innerHTML = 'Please provide a valid height'
@@ -18,6 +19,19 @@ form.addEventListener('submit',function (e) {
     else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2)
         results.innerHTML = `<span>${bmi}</span>`
+        if (bmi < 18.5) {
+            rr.innerHTML += `<span class="underweight">Underweight</span>`
+            
+        }
+        else if ((bmi >= 18.5) && (bmi <= 24.9)) {
+            rr.innerHTML += `<span class="normal">Normal</span>`
+        }
+        else if ((bmi >= 25) && (bmi <= 29.9)) {
+            rr.innerHTML += `<span class="overweight">Overweight</span>`
+        }
+        else {
+            rr.innerHTML += `<span class="obese">Obese</span>`
+        }
     }
 
 
