d98e339cf0a1c7754ff80c15105e4f64c30cf7e5
VISHAL <1316510155@kit.ac.in>
2024-01-13 21:53:28 +0530

    bmi

--- test/JS/AdvanceJS/ProjectThree/script.js
+++ test/JS/AdvanceJS/ProjectThree/script.js
@@ -1,1 +1,24 @@
 const form = document.querySelector('form');
+
+form.addEventListener('submit',function (e) {
+    e.preventDefault()
+
+
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if ((height === '') || (height < 0) || (isNaN(height))) {
+        document.querySelector('#height-error').innerHTML = 'Please provide a valid height'
+    }
+    else if ((weight === '') || (weight < 0) || (isNaN(weight))) {
+        document.querySelector('#height-error').innerHTML = 'Please provide a valid weight'
+    }
+    else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        results.innerHTML = `<span>${bmi}</span>`
+    }
+
+
+});
