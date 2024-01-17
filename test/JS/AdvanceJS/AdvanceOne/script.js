fddb0d7fbc0fce47358bc76f577345c518cdafe0
VISHAL <1316510155@kit.ac.in>
2024-01-17 20:52:28 +0530

    update

--- test/JS/AdvanceJS/AdvanceOne/script.js
+++ test/JS/AdvanceJS/AdvanceOne/script.js
@@ -1,5 +1,36 @@
-let heading = document.querySelector('h1');
-heading.style.color = 'red';
-let paragraph = document.getElementById('one').addEventListener('click',(e)=>{
-    console.log(e.timeStamp);
+// let heading = document.querySelector('h1');
+// heading.style.color = 'red';
+
+// let click = document.addEventListener('click',{
+
+// })
+
+document.addEventListener("DOMContentLoaded",function () {
+    
+    var style = document.createElement("style");
+    style.innerHTML=`
+    .clickDev{
+        position :absolute;
+        border :5px solid black;
+        border-radius:50%;
+        width:100px;
+        height:100px;
+    }
+    `;
+
+
+    document.head.appendChild(style);
+
+    document.addEventListener("click",function (e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        
+        var div = document.createElement("div");
+
+        div.className="clickDev";
+        div.style.left=x+"px";
+        div.style.top=y+"px";
+
+        document.body.appendChild(div);
+    })
 })
