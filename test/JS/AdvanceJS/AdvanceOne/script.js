0840ec5b8121df5cfc6b62d375e6bc040595f9a9
VISHAL <1316510155@kit.ac.in>
2024-01-17 10:59:36 +0530

    up

---
+++ test/JS/AdvanceJS/AdvanceOne/script.js
@@ -0,0 +1,5 @@
+let heading = document.querySelector('h1');
+heading.style.color = 'red';
+let paragraph = document.getElementById('one').addEventListener('click',(e)=>{
+    console.log(e.timeStamp);
+})
