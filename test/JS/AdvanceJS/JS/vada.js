a7a755f96219e708c8ddfb1fa272727c8ffe7096
VISHAL <1316510155@kit.ac.in>
2024-01-24 00:09:35 +0530

    promise

--- test/JS/AdvanceJS/JS/vada.js
+++ test/JS/AdvanceJS/JS/vada.js
@@ -1,22 +1,46 @@
-const promiseOne = new Promise(function (resolve,reject) {
+// const promiseOne = new Promise(function (resolve,reject) {
     
+//     setTimeout(() => {
+//         console.log('some async task is completed');
+//         resolve()
+//     }, 1000);
+// })
+
+// promiseOne.then(()=>{
+//     console.log("promise Consumed");
+// })
+
+
+// const promiseTwo = new Promise (function (resolve ,reject) {
+//     setTimeout(() => {
+//         resolve({username :"vishal", email:"vk@mail.com"})
+//     }, 1000);
+// })
+
+// promiseTwo.then(function (user) {
+//     console.log(user);
+// })
+
+
+const promiseThree = new Promise (function (resolve,reject) {
     setTimeout(() => {
-        console.log('some async task is completed');
-        resolve()
+        let error = false   
+        if(!error){
+            resolve({username:"vishal Kumar",password:"123@"})
+        }    else{
+            reject('ERROR:Something Went wrong')
+        }
     }, 1000);
 })
 
-promiseOne.then(()=>{
-    console.log("promise Consumed");
-})
+promiseThree.then((user)=>{
 
-
-const promiseTwo = new Promise (function (resolve ,reject) {
-    setTimeout(() => {
-        resolve({username :"vishal", email:"vk@mail.com"})
-    }, 1000);
-})
-
-promiseTwo.then(function (user) {
     console.log(user);
+    return user.username
+})
+.catch((error)=>{
+    console.log(error);
+})
+.finally(()=>{
+    console.log("This is Final");
 })
