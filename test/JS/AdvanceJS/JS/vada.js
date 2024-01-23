fb35f73ebe9715613c5e96736e299321331066e6
VISHAL <1316510155@kit.ac.in>
2024-01-24 00:30:15 +0530

    promise complete

--- test/JS/AdvanceJS/JS/vada.js
+++ test/JS/AdvanceJS/JS/vada.js
@@ -1,55 +1,46 @@
-// const promiseOne = new Promise(function (resolve,reject) {
+const promiseOne = new Promise(function (resolve,reject) {
     
-//     setTimeout(() => {
-//         console.log('some async task is completed');
-//         resolve()
-//     }, 1000);
-// })
+    setTimeout(() => {
+        console.log('some async task is completed');
+        resolve()
+    }, 1000);
+})
 
-// promiseOne.then(()=>{
-//     console.log("promise Consumed");
-// })
+promiseOne.then(()=>{
+    console.log("promise Consumed");
+})
 
 
-// const promiseTwo = new Promise (function (resolve ,reject) {
-//     setTimeout(() => {
-//         resolve({username :"vishal", email:"vk@mail.com"})
-//     }, 1000);
-// })
+const promiseTwo = new Promise (function (resolve ,reject) {
+    setTimeout(() => {
+        resolve({username :"vishal", email:"vk@mail.com"})
+    }, 1000);
+})
 
-// promiseTwo.then(function (user) {
-//     console.log(user);
-// })
+promiseTwo.then(function (user) {
+    console.log(user);
+})
 
 
 const promiseThree = new Promise (function (resolve,reject) {
     setTimeout(() => {
         let error = false   
         if(!error){
-            resolve({username:"vishal Kumar",password:"123@"})
+            resolve({username:"vishal-Kumar",password:"123@"})
         }    else{
             reject('ERROR:Something Went wrong')
         }
     }, 1000);
 })
 
-// promiseThree.then((user)=>{
-
-//     console.log(user);
-//     return user.username;
-// })
-// .catch((error)=>{
-//     console.log(error);
-// })
-// .finally(()=>{
-//     console.log("This is Final");
-// })
-
-
 promiseThree.then((user) => {
-    console.log(user);
-    return user.username; // Use user, not username
-})
+     console.log(user);
+    return user.username 
+}).then(
+    (username)=>{
+        console.log(username);
+    }
+)
 .catch((error) => {
     console.log(error);
 })
