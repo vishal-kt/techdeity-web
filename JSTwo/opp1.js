e63f12a8c65af3b4ac12925ae7b090956ddca4bc
VISHAL <1316510155@kit.ac.in>
2024-01-27 22:07:31 +0530

    update

--- JSTwo/opp1.js
+++ JSTwo/opp1.js
@@ -7,5 +7,17 @@ const user = {
         
     }
 }
-
 console.log(user.getUserName());
+
+const user2 = {
+    username:"vishal",
+    loginCount:5,
+    signedIn:true,
+    getUserName:function() {
+        return `Username is ${this.username} and his loginCount is ${this.loginCount}`
+        
+    }
+}
+
+const s = new user2
+console.log(s);
